6871f511b60b59b69a14332fce653bc6b5fb63fa
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-18 10:58:40 +0300

    Updated models.py

--- projects/models.py
+++ projects/models.py
@@ -2,5 +2,37 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    link = models.CharField(max_length=1000)
+    landingpic = models.ImageField(upload_to='images')
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+class Profile(models.Model):
+    dpicture = models.ImageField(upload_to='dp')
+    bio = models.CharField(max_length=50)
+    projectsposted = models.ForeignKey(Project)
+    contacts = models.CharField(max_length=12,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()
