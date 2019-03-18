a509ef874a1be6731d6f07d4e4dd34c963092db5
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-18 10:58:08 +0300

    Linked project.html to views

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,6 @@ from django.http  import HttpResponse
 # Create your views here.
 def landing(request):
     return render(request,'landing.html')
+
+def projects(request):
+    return render(request,'projects.html')    
