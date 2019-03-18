8d0bfe907b7d112d79b51bdfe4f0e5b21b4387bc
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-18 10:55:55 +0300

    Linked the projects url to be the main

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^projects/', include('projects.urls')),
+    url(r'', include('projects.urls')),
 ]
