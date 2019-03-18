278d3f2022d9a496fb2ed8a04d8fe321a093930f
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-18 10:59:41 +0300

    Updated admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Project,Profile
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
